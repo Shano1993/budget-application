41a850cf9b620b401905eb87682429a4a3edd707
Shano1993 <stefan.hanotiau@gmail.com>
2021-12-13 15:19:54 +0100

    budget

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,15 @@ let totalRecipes = [];
 let totalRec = document.getElementById('totalReci');
 let result = document.getElementById('saving');
 let suggestion = document.getElementById('suggest');
+let randomSuggestion = document.getElementById('randomSuggest');
+let random = "";
 let total1 = 0;
 let total2 = 0;
 let total3 = 0;
-
+let arrayPositive = ['a cars.', 'a game.', 'a house.', 'a salon.', 'a fridge.'];
+let arrayNegative = ['the outing !', 'the phone !', ' the internet !', 'a credit !'];
+let img1 = document.getElementById('yes');
+let img2 = document.getElementById('no');
 
 function pushExpenses() {
     for (let i = 0; i < expenses.length; i++) {
@@ -33,20 +38,26 @@ document.getElementById('total1').addEventListener("click", function () {
 
 document.getElementById('total2').addEventListener("click", function () {
     pushRecipes();
-})
+});
 
 document.getElementById('total3').addEventListener("click", function () {
     total3 = total2 - total1;
     result.innerHTML = total3 + " " + "Euros";
     saving();
-})
+});
 
 function saving() {
     if (total3 > 0) {
-        suggestion.innerHTML = 'Great ! You are positive' ;
+        suggestion.innerHTML = 'Great ! You are positive.' ;
+        random = arrayPositive[Math.floor(Math.random() * arrayPositive.length)];
+        randomSuggestion.innerHTML = 'You can buy yourself :' + " " + random;
+        img1.style.display = 'block';
     }
     if (total3 < 0) {
-        suggestion.innerHTML = 'Warning ! You are in negative ...';
+        suggestion.innerHTML = 'Warning ! You are in negative...';
+        random = arrayNegative[Math.floor(Math.random() * arrayNegative.length)];
+        randomSuggestion.innerHTML = 'Pay attention to :' + " " + random;
+        img2.style.display = 'block';
     }
 }
 
